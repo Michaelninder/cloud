96f814a63b066454811803170a30ed805d541244
Michaelninder <fabianeternis@gmail.com>
2025-08-11 14:34:39 +0200

    null

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -18,7 +18,7 @@
                 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                     <div class="flex items-center gap-4 rounded-xl bg-gray-100 dark:bg-gray-700 p-6 shadow hover:shadow-md transition duration-300">
                         <div class="flex h-12 w-12 items-center justify-center rounded-full bg-gray-300 dark:bg-gray-600">
-                            <i></i>
+                            <i class="bi bi-tag text-3xl text-gray-800 dark:text-gray-200"></i>
                         </div>
                         <div class="text-left">
                             <p class="text-sm font-medium text-gray-600 dark:text-gray-300">
@@ -27,7 +27,7 @@
                             <p class="text-2xl font-bold text-gray-900 dark:text-gray-100">
                                 {{ $stats['tags'] }}
                             </p>
-                            <a class="text-blue-700 hover:text-blue-800 dark:text-blue-300 dark:hover:text-blue-200 duration-[0.3456789s]" href="{{ route('cloud.tags.index') }}">SHow Tags</a>
+                            <a class="text-blue-700 hover:text-blue-800 dark:text-blue-300 dark:hover:text-blue-200 duration-[0.3456789s]" href="{{ route('cloud.tags.index') }}">Show Tags</a>
                         </div>
                     </div>
                 </div>
