c8d421fff5e8e4da09211af52139f5deb869e610
Michaelninder <fabianeternis@gmail.com>
2025-08-20 18:29:02 +0200

    feat: new layout-structure (section-based)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,27 +7,23 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
+            @hasSection('header')
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                        @yield('header')
                     </div>
                 </header>
-            @endisset
+            @endif
 
-            <!-- Session Status -->
             <div class="py-4 px-6">
             @if (session('status'))
                 <div class="bg-green-500 text-white p-4 rounded-lg shadow-md">
@@ -58,9 +54,8 @@
             @endif
             </div>
 
-            <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @yield('content')
             </main>
         </div>
     </body>
