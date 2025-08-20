f82c8c5dd069e426b8ff4502747019d9b9015a73
Michaelninder <fabianeternis@gmail.com>
2025-08-20 19:20:51 +0200

    UI

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,47 +13,60 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('cloud.tags.index')" :active="request()->routeIs('cloud.tags.*')">
-                        {{ __('Tags') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('cloud.files.index')" :active="request()->routeIs('cloud.files.*')">
-                        {{ __('Files') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('cloud.folders.index')" :active="request()->routeIs('cloud.folders.*')">
-                        {{ __('Folders') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('cloud.links.index')" :active="request()->routeIs('cloud.links.*')">
-                        {{ __('Links') }}
-                    </x-nav-link>
-                </div>
+                @auth
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('cloud.tags.index')" :active="request()->routeIs('cloud.tags.*')">
+                            {{ __('Tags') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('cloud.files.index')" :active="request()->routeIs('cloud.files.*')">
+                            {{ __('Files') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('cloud.folders.index')" :active="request()->routeIs('cloud.folders.*')">
+                            {{ __('Folders') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('cloud.links.index')" :active="request()->routeIs('cloud.links.*')">
+                            {{ __('Links') }}
+                        </x-nav-link>
+                    </div>
+                @endauth
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <img class="h-8 w-8 rounded-full object-cover mr-2" src="{{ Auth::user()->avatar() }}" alt="{{ Auth::user()->name }}">
-                            <div>{{ Auth::user()->name }}</div>
-                        </button>
-                    </x-slot>
+                @auth
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <img class="h-8 w-8 rounded-full object-cover mr-2" src="{{ Auth::user()->avatar() }}" alt="{{ Auth::user()->name }}">
+                                <div>{{ Auth::user()->name }}</div>
+                            </button>
+                        </x-slot>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')">
+                                {{ __('Profile') }}
                             </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                @else
+                    <x-nav-link :href="route('login')">
+                        {{ __('Log In') }}
+                    </x-nav-link>
+                    @if (Route::has('register'))
+                        <x-nav-link :href="route('register')">
+                            {{ __('Register') }}
+                        </x-nav-link>
+                    @endif
+                @endauth
             </div>
 
             <div class="-me-2 flex items-center sm:hidden">
@@ -72,38 +85,56 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('cloud.tags.index')" :active="request()->routeIs('cloud.tags.*')">
-                {{ __('Tags') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('cloud.files.index')" :active="request()->routeIs('cloud.files.*')">
-                {{ __('Files') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('cloud.folders.index')" :active="request()->routeIs('cloud.folders.*')">
-                {{ __('Folders') }}
-            </x-responsive-nav-link>
+            @auth
+                <x-responsive-nav-link :href="route('cloud.tags.index')" :active="request()->routeIs('cloud.tags.*')">
+                    {{ __('Tags') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('cloud.files.index')" :active="request()->routeIs('cloud.files.*')">
+                    {{ __('Files') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('cloud.folders.index')" :active="request()->routeIs('cloud.folders.*')">
+                    {{ __('Folders') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('cloud.links.index')" :active="request()->routeIs('cloud.links.*')">
+                    {{ __('Links') }}
+                </x-responsive-nav-link>
+            @endauth
         </div>
 
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
+            @auth
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                </div>
 
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('profile.edit')">
+                        {{ __('Profile') }}
                     </x-responsive-nav-link>
-                </form>
-            </div>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
+            @else
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('login')">
+                        {{ __('Log In') }}
+                    </x-responsive-nav-link>
+                    @if (Route::has('register'))
+                        <x-responsive-nav-link :href="route('register')">
+                            {{ __('Register') }}
+                        </x-responsive-nav-link>
+                    @endif
+                </div>
+            @endauth
         </div>
     </div>
 </nav>
