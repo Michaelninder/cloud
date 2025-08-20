a26da5d0ffbbfcf3fd0d3e67a203122d10f7cc31
Michaelninder <fabianeternis@gmail.com>
2025-08-20 18:49:13 +0200

    feat: Update UI and Add Link(s)

---
+++ app/Models/Link.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasUuid;
+
+class Link extends Model
+{
+    use HasUuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    
+    protected $fillable = [
+        'slug',
+        'original_url',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
