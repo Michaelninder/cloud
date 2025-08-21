c3c55d06e3c357c2d5cf14222a0e6429d8e37391
Michaelninder <fabianeternis@gmail.com>
2025-08-21 16:35:12 +0200

    feat: Update navigation(Icons and co.)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,22 +10,22 @@
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        <i class="bi bi-house-door-fill mr-2"></i>{{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
                 @auth
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                         <x-nav-link :href="route('cloud.tags.index')" :active="request()->routeIs('cloud.tags.*')">
-                            {{ __('Tags') }}
+                            <i class="bi bi-tags-fill mr-2"></i>{{ __('Tags') }}
                         </x-nav-link>
                         <x-nav-link :href="route('cloud.files.index')" :active="request()->routeIs('cloud.files.*')">
-                            {{ __('Files') }}
+                            <i class="bi bi-file-earmark-fill mr-2"></i>{{ __('Files') }}
                         </x-nav-link>
                         <x-nav-link :href="route('cloud.folders.index')" :active="request()->routeIs('cloud.folders.*')">
-                            {{ __('Folders') }}
+                            <i class="bi bi-folder-fill mr-2"></i>{{ __('Folders') }}
                         </x-nav-link>
                         <x-nav-link :href="route('cloud.links.index')" :active="request()->routeIs('cloud.links.*')">
-                            {{ __('Links') }}
+                            <i class="bi bi-link-45deg mr-2"></i>{{ __('Links') }}
                         </x-nav-link>
                     </div>
                 @endauth
@@ -38,32 +38,40 @@
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                                 <img class="h-8 w-8 rounded-full object-cover mr-2" src="{{ Auth::user()->avatar() }}" alt="{{ Auth::user()->name }}">
                                 <div>{{ Auth::user()->name }}</div>
+                                <div class="ms-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
                             </button>
                         </x-slot>
 
                         <x-slot name="content">
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                            </div>
                             <x-dropdown-link :href="route('profile.edit')">
-                                {{ __('Profile') }}
+                                <i class="bi bi-person-fill mr-2"></i>{{ __('Profile') }}
                             </x-dropdown-link>
-
+                            <div class="border-t border-gray-200 dark:border-gray-600"></div>
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-
                                 <x-dropdown-link :href="route('logout')"
                                         onclick="event.preventDefault();
                                                     this.closest('form').submit();">
-                                    {{ __('Log Out') }}
+                                    <i class="bi bi-box-arrow-right mr-2"></i>{{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
                         </x-slot>
                     </x-dropdown>
                 @else
                     <x-nav-link :href="route('login')">
-                        {{ __('Log In') }}
+                        <i class="bi bi-box-arrow-in-right mr-2"></i>{{ __('Log In') }}
                     </x-nav-link>
                     @if (Route::has('register'))
                         <x-nav-link :href="route('register')">
-                            {{ __('Register') }}
+                            <i class="bi bi-person-plus-fill mr-2"></i>{{ __('Register') }}
                         </x-nav-link>
                     @endif
                 @endauth
@@ -83,34 +91,37 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                <i class="bi bi-house-door-fill mr-2"></i>{{ __('Dashboard') }}
             </x-responsive-nav-link>
             @auth
                 <x-responsive-nav-link :href="route('cloud.tags.index')" :active="request()->routeIs('cloud.tags.*')">
-                    {{ __('Tags') }}
+                    <i class="bi bi-tags-fill mr-2"></i>{{ __('Tags') }}
                 </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('cloud.files.index')" :active="request()->routeIs('cloud.files.*')">
-                    {{ __('Files') }}
+                    <i class="bi bi-file-earmark-fill mr-2"></i>{{ __('Files') }}
                 </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('cloud.folders.index')" :active="request()->routeIs('cloud.folders.*')">
-                    {{ __('Folders') }}
+                    <i class="bi bi-folder-fill mr-2"></i>{{ __('Folders') }}
                 </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('cloud.links.index')" :active="request()->routeIs('cloud.links.*')">
-                    {{ __('Links') }}
+                    <i class="bi bi-link-45deg mr-2"></i>{{ __('Links') }}
                 </x-responsive-nav-link>
             @endauth
         </div>
 
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             @auth
-                <div class="px-4">
-                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="flex items-center px-4">
+                    <img class="h-9 w-9 rounded-full object-cover mr-2" src="{{ Auth::user()->avatar() }}" alt="{{ Auth::user()->name }}">
+                    <div>
+                        <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    </div>
                 </div>
 
                 <div class="mt-3 space-y-1">
                     <x-responsive-nav-link :href="route('profile.edit')">
-                        {{ __('Profile') }}
+                        <i class="bi bi-person-fill mr-2"></i>{{ __('Profile') }}
                     </x-responsive-nav-link>
 
                     <form method="POST" action="{{ route('logout') }}">
@@ -119,18 +130,18 @@
                         <x-responsive-nav-link :href="route('logout')"
                                 onclick="event.preventDefault();
                                             this.closest('form').submit();">
-                            {{ __('Log Out') }}
+                            <i class="bi bi-box-arrow-right mr-2"></i>{{ __('Log Out') }}
                         </x-responsive-nav-link>
                     </form>
                 </div>
             @else
                 <div class="mt-3 space-y-1">
                     <x-responsive-nav-link :href="route('login')">
-                        {{ __('Log In') }}
+                        <i class="bi bi-box-arrow-in-right mr-2"></i>{{ __('Log In') }}
                     </x-responsive-nav-link>
                     @if (Route::has('register'))
                         <x-responsive-nav-link :href="route('register')">
-                            {{ __('Register') }}
+                            <i class="bi bi-person-plus-fill mr-2"></i>{{ __('Register') }}
                         </x-responsive-nav-link>
                     @endif
                 </div>
